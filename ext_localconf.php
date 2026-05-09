449e8aad79827e030bf1084259bc6fb1f0d17521
Oliver Schlöbe <github@schloebe.de>
2026-05-09 14:19:44 +0200

    Remove icon registration from ext_localconf.php
    Already existing in /Configuration/Icons.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,5 @@
 <?php
+
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 use Joppnet\JnPhpcontentelement\Controller\JnPhpcontentController;
@@ -6,7 +7,6 @@ use Joppnet\JnPhpcontentelement\Controller\JnPhpcontentController;
 defined('TYPO3') or die();
 
 $boot = static function (): void {
-	$iconIdentifier = 'jnphpcontentelement-plugin-phpcelist';
 
 	ExtensionUtility::configurePlugin(
 		'JnPhpcontentelement',
@@ -19,14 +19,6 @@ $boot = static function (): void {
 		]
 	);
 
-	$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-	if (!$iconRegistry->isRegistered($iconIdentifier)) {
-		$iconRegistry->registerIcon(
-			$iconIdentifier,
-			\TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-			['source' => 'EXT:jn_phpcontentelement/Resources/Public/Icons/Extension.png']
-		);
-	}
 };
 
 $boot();
